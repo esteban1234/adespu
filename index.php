b48def947aa924ee0cd76db471695828ce6da329
esteban1234 <tva_libra@hotmail.com>
2017-08-17 16:01:55 -0500

    index listo y nosotros empieza

--- index.php
+++ index.php
@@ -17,12 +17,67 @@
   </nav>
 </header>
   <div class="banner">
-    
+    <p>SOLUCIONES EMPRESARIALES</p>
   </div>
- 
+ <div class="algunos">
+   <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-md-offset-2">
+            <img src="img/bufete.jpg"
+            class="img-responsive img-rounded">
+          </div>
+          <div class="col-md-5">
+            <h1 class="color" style="margin-top: 10%;">ADESPU SOLUCIONES EMPRESARIALES</h1>
+            <!-- <h3 class="color">A subtitle</h3> -->
+            <p class="texto">Empresa que brinda servicios jurídicos actuales e innovadores, que responden efectivamente a las necesidades de los clientes.</p>
+          </div>
+        </div>
 
- 
+      </div>
+      
+    </div>
+    <!-- <hr style="border: 2px inset #F28C00; height: 1px; width: 100%; "> -->
+ </div>
 
+ <div class="algunos2">
+   <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-md-offset-2">
+            <h1 class="text-left color">Clientes</h1>
+            <p class="tamano">
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Tecnología <br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Construcción<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Inmobiliaria<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Telecomunicaciones<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Servicios<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> entre otros..
+            </p>
+          </div>
+          <div class="col-md-6">
+            <h1 class="text-left color">Áreas de especialización</h1>
+            <p class="tamano">
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Derecho Corporativo<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Derecho Fiscal<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Contratos<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Inmigración<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> Contabilidad<br>
+              <i class="fa fa-cog fa-spin fa-1x fa-fw" aria-hidden="true"></i> entre otras..</p>
+          </div>
+        </div>
+      </div>
+    </div>
+ </div>
+
+  <div class="banner2">
+    <div class="fondo">
+      <p>Nuestra empresa le asegura a sus clientes la identificación de todas y cada una de las necesidades y exigencias legales, contables y administrativas que se le presenten, ofreciendo soluciones efectivas para su empresa y persona.</p>
+    </div>
+  </div>
+<div style="text-align: center; margin-top: 5%; margin-bottom: 5%;">
+      <a class="btn btn-warning btn-outline btn-lg" href="vistas/contacto.php">CONTACTANOS</a>
+    </div>
 
 
 <span class="ir-arriba"><i class="fa fa-chevron-up" aria-hidden="true"></i></span>
